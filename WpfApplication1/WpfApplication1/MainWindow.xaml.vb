e7d5a3bc97e420765cc1e5e73fde443fece349cb
jessontan <jessontan@yahoo.com>
2013-07-15 23:30:32 +0800

    15/07/2013

--- WpfApplication1/WpfApplication1/MainWindow.xaml.vb
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.vb
@@ -3,7 +3,13 @@
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles Button1.Click
         Try
             MessageBox.Show(Me.Label1.Content)
+
+
         Catch ex As Exception
         End Try
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles Button2.Click
+        MessageBox.Show("Nice Work")
     End Sub
 End Class
