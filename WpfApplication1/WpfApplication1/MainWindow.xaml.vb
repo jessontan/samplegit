29c36dbce940410ca8e53f3f78aacaf04f69b8f5
jessontan <jessontan@yahoo.com>
2013-07-15 23:20:36 +0800

    this is my first project

---
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.vb
@@ -0,0 +1,9 @@
+﻿Class MainWindow 
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles Button1.Click
+        Try
+            MessageBox.Show(Me.Label1.Content)
+        Catch ex As Exception
+        End Try
+    End Sub
+End Class
